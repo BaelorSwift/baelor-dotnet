d9736a37d0fb55a4bb6ce9bed8564b83d6f898c1
Alex Forbes-Reed <alexreed@outlook.com>
2015-09-02 15:01:35 +0100

    updated build and test command files

--- build.cmd
+++ build.cmd
@@ -1,3 +1,4 @@
 set PATH=C:\Program Files (x86)\MSBuild\14.0\Bin;%PATH%
 dnvm install 1.0.0-beta6
-dir
+dnu restore C:\projects\csharp-library\src\BaelorNet
+dnu build C:\projects\csharp-library\src\BaelorNet
--- test.cmd
+++ test.cmd
@@ -1,1 +1,3 @@
+dnu restore C:\projects\csharp-library\test\BaelorNet.Tests
+dnu build C:\projects\csharp-library\test\BaelorNet.Tests
 dnx C:\projects\csharp-library\test\BaelorNet.Tests\ test
