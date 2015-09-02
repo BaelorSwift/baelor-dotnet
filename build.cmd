3903579001aa1edf4accb131c1a891eda45df9a8
Alex Forbes-Reed <alexreed@outlook.com>
2015-09-02 13:49:41 +0100

    updated build script

--- build.cmd
+++ build.cmd
@@ -1,32 +1,5 @@
-@echo off
-cd %~dp0
+set PATH=C:\Program Files (x86)\MSBuild\14.0\Bin;%PATH%
 
-SETLOCAL
-SET CACHED_NUGET=%LocalAppData%\NuGet\NuGet.exe
+dnvm install 1.0.0-beta6
 
-IF EXIST %CACHED_NUGET% goto copynuget
-echo Downloading latest version of NuGet.exe...
-IF NOT EXIST %LocalAppData%\NuGet md %LocalAppData%\NuGet
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest 'https://www.nuget.org/nuget.exe' -OutFile '%CACHED_NUGET%'"
-
-:copynuget
-IF EXIST .nuget\nuget.exe goto restore
-md .nuget
-copy %CACHED_NUGET% .nuget\nuget.exe > nul
-
-:restore
-IF EXIST packages\KoreBuild goto run
-.nuget\NuGet.exe install KoreBuild -ExcludeVersion -o packages -nocache -pre
-.nuget\NuGet.exe install Sake -ExcludeVersion -Source https://www.nuget.org/api/v2/ -Out packages
-
-IF "%1" == "rebuild-package" goto run
-
-IF "%SKIP_DNX_INSTALL%"=="1" (
-    REM On the CI, don't upgrade since the previous installed DNVM is already there.
-    CALL packages\KoreBuild\build\dnvm use default -runtime CLR -arch x86
-) ELSE (
-    CALL packages\KoreBuild\build\dnvm upgrade -runtime CLR -arch x86
-)
-
-:run
-packages\Sake\tools\Sake.exe -I packages\KoreBuild\build -f makefile.shade %*
+dnx .\test\BaelorNet.Tests\ test
